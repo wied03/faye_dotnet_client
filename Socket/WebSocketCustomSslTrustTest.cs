d4260443385f9b34e85f8d021eb1013a3aa5054e
Brady Wied <brady@wied.us>
2013-07-19 10:43:33 -0500

    Added base test infrastructure (including ruby files)

---
+++ Socket/WebSocketCustomSslTrustTest.cs
@@ -0,0 +1,71 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using FluentAssertions;
+using Bsw.WebSocket4NetSslExt.Socket;
+
+#endregion
+
+namespace Bsw.WebSocket4NetSslExt.Test.Socket
+{
+    [TestFixture]
+    public class WebSocketCustomSslTrustTest : BaseTest
+    {
+        private WebSocketCustomSslTrust _socket;
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _socket = new WebSocketCustomSslTrust(uri: "localhost");
+        }
+
+        [Test]
+        public void Cant_connect()
+        {
+            // arrange
+
+            // act + assert
+            _socket.Invoking(s => s.Open())
+                .ShouldThrow<ConnectionException>()
+                ;
+           Assert.Fail("finish test");
+        }
+
+        [Test]
+        public void Not_ssl()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.Fail("write test");
+        }
+
+        [Test]
+        public void Ssl_but_not_trusted_by_us()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.Fail("write test");
+        }
+
+        [Test]
+        public void Ssl_trusted_by_us()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.Fail("write test");
+        }
+    }
+}
---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'faye/websocket'
+
+puts "Loading websocket..."
+
+App = lambda do |env|
+  if Faye::WebSocket.websocket?(env)
+      ws = Faye::WebSocket.new(env,"isonas")
+
+      ws.on :message do |event|
+        puts "got message '#{event.data}'"       
+        ws.send("Received your message "+event.data)
+      end
+
+      ws.on :close do |event|
+        p [:close, event.code, event.reason]
+        ws = nil
+      end
+
+      ws.on :open do |event|
+        puts "Socket opened, awaiting authentication"
+      end
+
+      # Return async Rack response
+      ws.rack_response
+
+    else
+      # Normal HTTP request
+      [200, {'Content-Type' => 'text/plain'}, ['Hello']]
+    end
+end
