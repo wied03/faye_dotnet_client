04bb3aa1eb96f114f30600d3965de812224e15ce
Brady Wied <brady@wied.us>
2013-07-19 10:28:21 -0500

    Added test project and basic class

---
+++ Socket/WebSocketCustomSslTrust.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using WebSocket4Net;
+
+#endregion
+
+namespace Bsw.WebSocket4NetSslExt.Socket
+{
+    public class WebSocketCustomSslTrust : WebSocket
+    {
+        public WebSocketCustomSslTrust(string uri,
+            string subProtocol,
+            WebSocketVersion version) : base(uri,
+                subProtocol,
+                version)
+        {
+        }
+    }
+}
