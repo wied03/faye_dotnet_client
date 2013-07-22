6e87595b152968df89ac99fe000c7387a2477ca8
Brady Wied <brady@wied.us>
2013-07-21 21:46:52 -0500

    Added more test code and expected exception

---
+++ Socket/SSLException.cs
@@ -0,0 +1,14 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+#endregion
+
+namespace Bsw.WebSocket4NetSslExt.Socket
+{
+    public class SSLException : Exception
+    {
+    }
+}
