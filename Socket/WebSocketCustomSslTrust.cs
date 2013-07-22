db8cf0f43b5fbf794eceeb1da06bd065f65b8751
Brady Wied <brady@wied.us>
2013-07-21 21:31:41 -0500

    * Find kill process that works (don't use thin batch file) * Add an interface to prepare for future integration

--- Socket/WebSocketCustomSslTrust.cs
+++ Socket/WebSocketCustomSslTrust.cs
@@ -11,8 +11,11 @@ using WebSocket4Net;
 
 namespace Bsw.WebSocket4NetSslExt.Socket
 {
-    public class WebSocketCustomSslTrust : WebSocket
+    public class WebSocketCustomSslTrust : WebSocket,
+                                           IWebSocket
     {
+        private readonly IEnumerable<X509Certificate> _trustedCertChain;
+
         /// <summary>
         ///     Constructs a websocket that verifies the server's certificate is signed by someone we explicitly trust
         ///     before we open the websocket
@@ -21,11 +24,17 @@ namespace Bsw.WebSocket4NetSslExt.Socket
         /// <param name="trustedCertChain">
         ///     List of certificates that should be trusted.  If any certificate in the server's chain
         ///     matches these certs, the verification will pass.  Otherwise a ConnectionException is thrown
-        /// when Open() is called.
+        ///     when Open() is called.
         /// </param>
         public WebSocketCustomSslTrust(string uri,
-            IEnumerable<X509Certificate> trustedCertChain) : base(uri)
+                                       IEnumerable<X509Certificate> trustedCertChain) : base(uri)
         {
+            _trustedCertChain = trustedCertChain;
+        }
+
+        public new void Open()
+        {
+            base.Open();
         }
     }
 }
