33505565a96d4660c1a1c85dfe8aafd943e56c26
Brady Wied <brady@wied.us>
2013-07-29 11:17:12 -0500

    Add basic message code and test code

--- Client/FayeClientTest.cs
+++ Client/FayeClientTest.cs
@@ -1,12 +1,19 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Bsw.FayeDotNet.Client;
+using Bsw.FayeDotNet.Messages;
 using Bsw.WebSocket4NetSslExt.Socket;
+using MsbwTest;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using FluentAssertions;
+using Rhino.Mocks;
 
 #endregion
 
@@ -15,29 +22,95 @@ namespace Bsw.FayeDotNet.Test.Client
     [TestFixture]
     public class FayeClientTest : BaseTest
     {
-        private IFayeClient _client;
+        #region Test Fields
+        private IWebSocket _websocket;
+        private List<string> _messagesSent; 
+        #endregion
+
+        #region Setup/Teardown
 
         [SetUp]
         public override void SetUp()
         {
             base.SetUp();
-            var websocket = new WebSocketClient(uri: "ws://weez.weez.wied.us:8000");
-            _client = new FayeClient(websocket);
+            _messagesSent = new List<string>();
+        } 
+
+        #endregion
+
+        #region Utility Methods
+        private async Task<IFayeClient> GetFayeClient()
+        {
+            var client = new FayeClient(_websocket);
+            await client.Connect();
+            return client;
         }
+
+        private void SetupWebSocket(IWebSocket webSocket)
+        {
+            _websocket = webSocket;
+        }
+
+        private string GetHandshakeResponse(bool successful = true)
+        {
+            var response =
+                new
+                {
+                    channel = HandshakeRequestMessage.HANDSHAKE_MESSAGE,
+                    version = HandshakeRequestMessage.BAYEUX_VERSION_1, 
+                    successful
+                };
+            return JsonConvert.SerializeObject(response);
+        } 
+        #endregion
+
+        #region Tests
 
         [Test]
         public void Connect_wrong_connectivity_info()
         {
             // arrange
+            SetupWebSocket(new WebSocketClient(uri: "ws://foobar:8000"));
 
-            // act
+            // act + assert
+            this.InvokingAsync(t => t.GetFayeClient())
+                .ShouldThrow<ArgumentNullException>();
+            Assert.Fail("Fix the exception type");
+        }
+
+        [Test]
+        public void Connect_websocketopens_but_handshake_fails()
+        {
+            // arrange
+            var mockSocket = new MockSocket
+                             {
+                                 OpenedAction = handler =>
+                                                {
+                                                    Thread.Sleep(100);
+                                                    handler.Invoke(this,
+                                                                   new EventArgs());
+                                                },
+                                 MessageSentAction = msg => _messagesSent.Add(msg),
+                                 MessageReceiveAction = () =>
+                                                        {
+                                                            Thread.Sleep(100);
+                                                            return GetHandshakeResponse(successful:false);
+                                                        }
+                             };
+
+            SetupWebSocket(mockSocket);
+
+            // act + assert
+            this.InvokingAsync(t => t.GetFayeClient())
+                .ShouldThrow<Exception>();
+                //.WithMessage("Should be some exception indicating the handshake failed");
 
             // assert
             Assert.Fail("write test");
         }
 
         [Test]
-        public void Connect_websocketopens_but_handshake_fails()
+        public void Connect_websocketopens_but_handshake_times_out()
         {
             // arrange
 
@@ -49,6 +122,17 @@ namespace Bsw.FayeDotNet.Test.Client
 
         [Test]
         public void Connect_handshake_completes()
+        {
+            // arrange
+
+            // act
+
+            // assert
+            Assert.Fail("write test");
+        }
+
+        [Test]
+        public void Connect_lost_connection_retry_happens_properly()
         {
             // arrange
 
@@ -90,5 +174,7 @@ namespace Bsw.FayeDotNet.Test.Client
             // assert
             Assert.Fail("write test");
         }
+
+        #endregion
     }
 }
---
+++ Client/MockSocket.cs
@@ -0,0 +1,78 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Bsw.WebSocket4NetSslExt.Socket;
+using SuperSocket.ClientEngine;
+using WebSocket4Net;
+
+#endregion
+
+namespace Bsw.FayeDotNet.Test.Client
+{
+    public class MockSocket : IWebSocket
+    {
+        public Action<EventHandler> OpenedAction;
+        public Action<string> MessageSentAction;
+        public Func<string> MessageReceiveAction;
+
+        public void Open()
+        {
+            OpenedAction(Opened);
+        }
+
+        public void Send(string message)
+        {
+            MessageSentAction(message);
+            Task.Factory.StartNew(() => MessageReceived(this,
+                                                        new MessageReceivedEventArgs(MessageReceiveAction.Invoke())));
+        }
+
+        public void Send(byte[] data,
+                         int offset,
+                         int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(IList<ArraySegment<byte>> segments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(string reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close(int statusCode,
+                          string reason)
+        {
+            throw new NotImplementedException();
+        }
+
+        public WebSocketVersion Version { get; private set; }
+        public DateTime LastActiveTime { get; private set; }
+        public bool EnableAutoSendPing { get; set; }
+        public int AutoSendPingInterval { get; set; }
+        public bool SupportBinary { get; private set; }
+        public WebSocketState State { get; private set; }
+        public bool Handshaked { get; private set; }
+        public IProxyConnector Proxy { get; set; }
+        public int ReceiveBufferSize { get; set; }
+        public bool AllowUnstrustedCertificate { get; set; }
+        public event EventHandler Opened;
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+        public event EventHandler Closed;
+        public event EventHandler<ErrorEventArgs> Error;
+    }
+}
