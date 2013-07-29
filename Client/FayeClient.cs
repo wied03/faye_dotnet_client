2585b1d2833d113251cd253bfd7dffa9438b6a91
Brady Wied <brady@wied.us>
2013-07-28 22:35:01 -0500

    Add straw man structure and unit test

---
+++ Client/FayeClient.cs
@@ -0,0 +1,41 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Bsw.WebSocket4NetSslExt.Socket;
+
+#endregion
+
+namespace Bsw.FayeDotNet.Client
+{
+    public class FayeClient : IFayeClient
+    {
+        public FayeClient(IWebSocket socket)
+        {
+            
+        }
+
+        public void Disconnect()
+        {
+            
+        }
+
+        public void Subscribe(string channel,
+                              Action<object> messageReceived)
+        {
+            
+        }
+
+        public void Unsubscribe(string channel)
+        {
+            
+        }
+
+        public void Publish(string channel,
+                            object message)
+        {
+            
+        }
+    }
+}
---
+++ Client/IFayeClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bsw.FayeDotNet.Client
+{
+    public interface IFayeClient
+    {
+        void Disconnect();
+
+        void Subscribe(string channel,
+                                       Action<object> messageReceived);
+
+        void Unsubscribe(string channel);
+
+        void Publish(string channel,
+                                     object message);
+    }
+}
