11cf3eabed19d343919a7c825ab4f2255bea0067
Brady Wied <brady@wied.us>
2013-07-29 19:15:50 -0500

    Added basic disconnect test

---
+++ Client/FayeConnectionException.cs
@@ -0,0 +1,17 @@
+﻿#region
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+#endregion
+
+namespace Bsw.FayeDotNet.Client
+{
+    public class FayeConnectionException : Exception
+    {
+        public FayeConnectionException(string message) : base(message)
+        {
+        }
+    }
+}
